03bd76c3d4360d9ee8d0b27c0938d3b08688aaf8
motioninstraitline7761 <motioninstraitline7761@gmail.com>
2025-07-21 03:40:31 +0530

    Add files via upload

--- vars.py
+++ vars.py
@@ -3,10 +3,10 @@
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "28585138"))
-API_HASH = environ.get("API_HASH", "74b7afde7d9a546db3451cf4689f4e14")
+API_ID = int(environ.get("API_ID", "28094744"))
+API_HASH = environ.get("API_HASH", "a75af4285edc7747c57bb19147ca0b9b")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
-OWNER = int(environ.get("OWNER", "7181075926"))
+OWNER = int(environ.get("OWNER", "5680454765"))
 CREDIT = "𝄟⃝‌🐬🇳‌ɪᴋʜɪʟ𝄟⃝🐬"
 #WEBHOOK = True  # Don't change this
 #PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
